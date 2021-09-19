154b16cb5198072858fcdba656ba347448748d92
Claylson-0 <64693150+Claylson-0@users.noreply.github.com>
2021-09-19 16:41:12 -0300

    Desafio aula 02

--- aula_02/javascript.js
+++ aula_02/javascript.js
@@ -1,11 +1,65 @@
 function Converter(){
-    var valorElemento = document.getElementById("valor");
-    var valor = valorElemento.value;
-    var valorEmDolarNumerico= parseFloat(valor);
-
-    var valorEmReal= valorEmDolarNumerico*5;
+    var moeda = document.getElementById("moeda").value;
+    var valor= parseFloat(document.getElementById("valor").value);
+    var resultado;
+    if(moeda=="dólar"){
+        resultado="O valor em dólar é "+(valor/5.29).toFixed(2);
+    }else if(moeda=="euro"){
+        resultado="O valor em euro é "+(valor/6.20).toFixed(2);
+    }else if(moeda=="bitcoin"){
+        resultado="O valor em bitcoin é "+valor/250704.40;
+    }else{
+        resultado="Moeda não identificada"
+    }
 
     var elementoValorConvertido= document.getElementById("valorConvertido");
-    var valorConvertido="O resultado em real é R$ "+valorEmReal;
-    elementoValorConvertido.innerHTML=valorConvertido;
+    elementoValorConvertido.innerHTML=resultado;
+}
+
+function anoLuz(){
+    var km= parseFloat(document.getElementById("km").value);
+    var anoLuz= km/9.461e+12;
+    var resultado=document.getElementById("anoLuzConvertido");
+    resultado.innerHTML=anoLuz;
+}
+
+function temperatura(){
+    var valorTemp= parseFloat(document.getElementById("valorTemp").value);
+    var entradaTemp= document.getElementById("entradaTemp").value;
+    var saidaTemp= document.getElementById("saidaTemp").value;
+    var temConvertido;
+    if(entradaTemp=="farenheit"){
+        if(saidaTemp=="farenheit"){
+            temConvertido=valorTemp;
+        }else if(saidaTemp=="kelvin"){
+            temConvertido=((valorTemp-32)*(5/9))+273;
+        }else if(saidaTemp=="celcius"){
+            temConvertido=(valorTemp-32)/1.8;
+        }else{
+            temConvertido="Saida de temperatura invalida";
+        }
+    }else if(entradaTemp=="kelvin"){
+        if(saidaTemp=="farenheit"){
+        temConvertido=((valorTemp-273)*1.8)+32;
+        }else if(saidaTemp=="kelvin"){
+            temConvertido=valorTemp;
+        }else if(saidaTemp=="celcius"){
+            temConvertido=-(valorTemp-273);
+        }else{
+            temConvertido="Saida de temperatura invalida";
+        }
+    }else if(entradaTemp=="celcius"){
+        if(saidaTemp=="farenheit"){
+            temConvertido=(1.8*valorTemp)+32;
+        }else if(saidaTemp=="kelvin"){
+            temConvertido=valorTemp+273;
+        }else if(saidaTemp=="celcius"){
+            temConvertido=valorTemp;
+        }else{
+            temConvertido="Saida de temperatura invalida";
+        }
+    }else{
+        temConvertido="Entrada de temperatura invalida";
+    }
+    document.getElementById("temConvertido").innerHTML=temConvertido;
 }
