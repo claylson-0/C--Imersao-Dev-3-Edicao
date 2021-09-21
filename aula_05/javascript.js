67bc4bbbb8a97fadd3a28fc44d43536e69146078
Claylson-0 <64693150+Claylson-0@users.noreply.github.com>
2021-09-21 16:44:02 -0300

    Desafio aula 05

--- aula_05/javascript.js
+++ aula_05/javascript.js
@@ -1,15 +1,42 @@
+var listaNome = ["boku no hero", "darling in the franxx"];
+var listaFilme = ["https://i.pinimg.com/originals/e9/76/a2/e976a29e0ffaefd0c16bc2be934cd9dd.jpg", "https://64.media.tumblr.com/c9a6e9a6c8ba697f97b909ee59a508b1/tumblr_p7taf5vQkp1rovzi7o1_1280.jpg"];
+
 function adicionarFilme() {
     var filmeFavorito = document.getElementById("filme").value;
+    var nomeFilme = document.getElementById("nome").value;
+
     if (filmeFavorito.endsWith(".jpg")) {
-     listarFilmeNaTela(filmeFavorito);
-    }else {
+        listaNome.push(nomeFilme);
+        listaFilme.push(filmeFavorito);
+
+        listarFilmeNaTela();
+    } else {
         console.error("Endereço de filme inválido");
     }
-    filmeFavorito.value = "";
     document.getElementById("filme").value = "";
+    document.getElementById("nome").value = "";
 }
-function listarFilmeNaTela(filme){
-    var elementoFilmeFavorito = "<img src=" + filme + ">";
+function listarFilmeNaTela() {
+    var elementoNomeFilme = ""
     var elementoListaFilmes = document.getElementById("listaFilmes");
-    elementoListaFilmes.innerHTML = elementoFilmeFavorito + elementoListaFilmes;
+    for (var i = 0; i < listaNome.length; i++) {
+        elementoNomeFilme += "<h2>" + listaNome[i] + "</h2><br><img src=" + listaFilme[i] + ">";
+        elementoListaFilmes.innerHTML = elementoNomeFilme;
+    }
+    if(listaNome.length==0){
+        elementoListaFilmes.innerHTML=""
+    }
+}
+listarFilmeNaTela();
+function deletarFilme() {
+    var nomeDeletar=document.getElementById("nomeDeletar").value;
+    for(var i =0;i<listaNome.length;i++){
+        if(nomeDeletar==listaNome[i]){
+            listaNome.splice(i,1);
+            listaFilme.splice(i,1);
+            
+            listarFilmeNaTela()
+        }
+    }
+    document.getElementById("nomeDeletar").value="";
 }
